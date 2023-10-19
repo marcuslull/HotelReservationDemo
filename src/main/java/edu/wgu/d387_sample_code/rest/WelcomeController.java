1271e74dde613d338a74c18bb59b917cb7e9f7f7
Marcus Lull <marcuslull@gmail.com>
2023-10-19 10:30:53 -0700

    Requirement Part B1b - MultiThreading: Adding a subclass to the WelcomeController to handle the runnable interface, and spin up new threads.

--- src/main/java/edu/wgu/d387_sample_code/rest/WelcomeController.java
+++ src/main/java/edu/wgu/d387_sample_code/rest/WelcomeController.java
@@ -17,8 +17,39 @@ public class WelcomeController {
         Locale enLocale = Locale.CANADA;
         Locale frLocale = Locale.CANADA_FRENCH;
 
-        ResourceBundle message = ResourceBundle.getBundle("D387_sample_code", frLocale);
+        WelcomeRunnable runnable1 = new WelcomeRunnable(enLocale);
+        WelcomeRunnable runnable2 = new WelcomeRunnable(frLocale);
 
-        return message.getString("welcome");
+        Thread thread1 = new Thread(runnable1);
+        Thread thread2 = new Thread(runnable2);
+
+        thread1.start();
+        thread2.start();
+
+        while(thread1.isAlive() || thread2.isAlive()) {
+            // wait for both threads to complete
+        }
+        return thread1.getName() + ": " + runnable1.getWelcomeMessage() + " " + thread2.getName() + ": " + runnable2.getWelcomeMessage();
+    }
+
+    public static class WelcomeRunnable implements Runnable {
+
+        Locale currentLocale;
+        String welcomeMessage;
+
+        public WelcomeRunnable(Locale currentLocale) {
+            this.currentLocale = currentLocale;
+        }
+
+        public String getWelcomeMessage() {
+            return welcomeMessage;
+        }
+
+        @Override
+        public void run() {
+            ResourceBundle message = ResourceBundle.getBundle("D387_sample_code", currentLocale);
+            welcomeMessage = message.getString("welcome");
+        }
     }
 }
+
